9e290fa0daee75df2b3c4f4f49685ca3052d9b7e
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-08 00:43:14 +0300

    add Sidebar structure

---
+++ app/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import styles from './Sidebar.module.scss'
+
+const Sidebar: FC = () => {
+	return (
+		<div>
+			<h1>Sidebar</h1>
+		</div>
+	)
+}
+
+export default Sidebar
