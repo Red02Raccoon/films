c411a2e9c7f37f35aa6aae05c1a1b86be5a672f7
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-08 00:42:55 +0300

    add Navigation

---
+++ app/components/layout/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import styles from './Navigation.module.scss'
+
+const Navigation: FC = () => {
+	return (
+		<div>
+			<h1>Navigation</h1>
+		</div>
+	)
+}
+
+export default Navigation
