e420d0c92a045c7c0b064732af2c5c551eee307e
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-09 00:45:58 +0300

    start with services

--- app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
+++ app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
@@ -1,1 +1,27 @@
-export const usePopularGenres = () => {}
+import { useQuery } from 'react-query'
+
+import { GenreService } from '@/services/genre.service'
+
+import { getGenresUrl } from '@/config/api.config'
+
+import { IMenuItem } from '../menu.interface'
+
+export const usePopularGenres = () => {
+	const queryData = useQuery('some unique key', () => GenreService.getAll(), {
+		select: ({ data }) =>
+			data
+				.map(
+					(genre) =>
+						({
+							icon: genre.icon,
+							link: getGenresUrl(genre.slug),
+							tittle: genre.name,
+						} as IMenuItem)
+				)
+				.splice(0, 4),
+
+		onError(error) {},
+	})
+
+	return queryData
+}
---
+++ app/services/genre.service.ts
@@ -0,0 +1,15 @@
+import { axiosClassic } from 'api/interceptors'
+
+import { IMenuItem } from '@/components/layout/Navigation/MenuContainer/menu.interface'
+
+import { IGenre } from '@/shared/types/movie.types'
+
+import { getGenresUrl } from '@/config/api.config'
+
+export const GenreService = {
+	async getAll(searchTerm?: string) {
+		return axiosClassic.get<IGenre>(getGenresUrl(''), {
+			params: searchTerm ? { searchTerm } : {},
+		})
+	},
+}
